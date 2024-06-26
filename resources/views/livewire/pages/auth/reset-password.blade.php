cfa016d2897a091f478fa3ec0069199d67d5fd46
ozonerik <ozonerik@gmail.com>
2024-06-26 11:54:00 +0700

    reset password fix

--- resources/views/livewire/pages/auth/reset-password.blade.php
+++ resources/views/livewire/pages/auth/reset-password.blade.php
@@ -69,37 +69,40 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <form wire:submit="resetPassword">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                          type="password"
-                          name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</div>
+<div class="login-box">
+    <div class="login-logo"> <a href="{{ url('/') }}"><b>Admin</b>LTE</a> </div> <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </div>
+            <!-- Session Status -->
+            <x-auth-session-status :status="session('status')" />
+            <form wire:submit="resetPassword">
+                <div class="input-group mb-3"> <input type="email" wire:model="email" id="email" class="form-control @error('form.email') is-invalid @enderror" placeholder="Email" name="email" required autofocus autocomplete="username">
+                    <div class="input-group-text"> <span class="bi bi-envelope"></span> </div>
+                    @error('email')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="input-group mb-3"> <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" wire:model="password" id="password" name="password" required autocomplete="new-password">
+                    <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                    @error('password')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="input-group mb-3"> <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" placeholder="Confirm Password" wire:model="password_confirmation" id="password_confirmation" name="password_confirmation" required autocomplete="new-password">
+                    <div class="input-group-text"> <span class="bi bi-lock-fill"></span> </div>
+                    @error('password_confirmation')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div> <!--begin::Row-->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="d-grid gap-2"> <button type="submit" class="btn btn-primary">{{ __('Reset Password') }}</button> </div>
+                    </div> <!-- /.col -->
+                </div> <!--end::Row-->
+            </form>
+        </div> <!-- /.login-card-body -->
+    </div>
+</div> <!-- /.login-box --> <!--begin::Third Party Plugin(OverlayScrollbars)-->
