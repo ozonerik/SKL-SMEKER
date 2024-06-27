1827f6caf21f995df88921af73961d290ea40909
ozonerik <ozonerik@gmail.com>
2024-06-27 22:07:17 +0700

    change aut-session-status

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,7 +1,7 @@
 @props(['status'])
 
 @if ($status)
-    <div {{ $attributes->merge(['class' => 'callout callout-info']) }}>
+    <div {{ $attributes->merge(['class' => 'callout callout-info mb-3 mt-2']) }}>
         {{ $status }}
     </div>
 @endif
