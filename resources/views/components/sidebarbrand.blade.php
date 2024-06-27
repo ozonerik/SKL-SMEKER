399d783ceddc3331c572592f7524330a5f2f84b1
ozonerik <ozonerik@gmail.com>
2024-06-27 22:47:33 +0700

    sidebrand component

---
+++ resources/views/components/sidebarbrand.blade.php
@@ -0,0 +1,6 @@
+<div class="sidebar-brand">
+    <a href="{{ url('/') }}" class="brand-link" wire:navigate>
+        <img src="{{ asset('dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image opacity-75 shadow">
+        <span class="brand-text fw-light">AdminLTE 4</span> 
+    </a>
+</div>
