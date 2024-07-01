d1bab7f605c9e2b54c568bf50015d80d868b7ece
ozonerik <ozonerik@gmail.com>
2024-07-01 23:53:06 +0700

    sidebar fixed

--- resources/views/livewire/layout/sidebarlock.blade.php
+++ resources/views/livewire/layout/sidebarlock.blade.php
@@ -4,27 +4,12 @@
         <nav class="mt-2"> <!--begin::Sidebar Menu-->
             <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
                 @if(in_array(Route::currentRouteName(),['verification.notice']))
-                <li class="nav-item"> 
-                    <a href="{{ route('verification.notice') }}" class="nav-link {{ request()->routeIs('verification.notice')?'active':'' }}" wire:navigate> 
-                        <i class="nav-icon bi bi-envelope-check-fill"></i>
-                        <p> Verification Email </p>
-                    </a>
-                </li>
+                <x-ui.navitem label="Verification Email" link="verification.notice" icon="bi bi-envelope-check-fill" />
                 @endif
                 @if(in_array(Route::currentRouteName(),['password.confirm']))
-                <li class="nav-item"> 
-                    <a href="{{ route('password.confirm') }}" class="nav-link {{ request()->routeIs('password.confirm')?'active':'' }}" wire:navigate> 
-                        <i class="nav-icon bi bi-key-fill"></i>
-                        <p> Restricted Access</p>
-                    </a>
-                </li>
+                <x-ui.navitem label="Restricted Access" link="password.confirm" icon="bi bi-key-fill" />
                 @endif
-                <li class="nav-item"> 
-                    <a href="{{ route('profilelock') }}" class="nav-link {{ request()->routeIs('profilelock')?'active':'' }}" wire:navigate> 
-                        <i class="nav-icon bi bi-person-circle"></i>
-                        <p> Profile </p>
-                    </a>
-                </li>
+                <x-ui.navitem label="Profile" link="profilelock" icon="bi bi-person-circle" />
             </ul> <!--end::Sidebar Menu-->
         </nav>
     </div> <!--end::Sidebar Wrapper-->
