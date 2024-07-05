ad2477fa5e080dd0e09633bd442fa558f845be95
ozonerik <ozonerik@gmail.com>
2024-07-05 10:14:14 +0700

    datatable checkbox

--- resources/views/components/ui/datatables.blade.php
+++ resources/views/components/ui/datatables.blade.php
@@ -1,10 +1,13 @@
 @assets
 <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css" />
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.css" />
+<link rel="stylesheet" href="https://cdn.datatables.net/select/2.0.3/css/select.bootstrap5.css" />
 <script data-navigate-once src="//cdn.datatables.net/2.0.8/js/dataTables.min.js" ></script>
 <script data-navigate-once src="//cdn.datatables.net/2.0.8/js/dataTables.bootstrap5.js" ></script>
 <script data-navigate-once src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.js" ></script>
 <script data-navigate-once src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.bootstrap5.js" ></script>
+<script data-navigate-once src="https://cdn.datatables.net/select/2.0.3/js/dataTables.select.js" ></script>
+<script data-navigate-once src="https://cdn.datatables.net/select/2.0.3/js/select.bootstrap5.js" ></script>
 @endassets
 
 @props([
@@ -22,21 +25,35 @@ $cell=collect(['name','email']);
     let table = new DataTable('#{{ $id }}', {
         retrieve: true,
         paging: true,
+        select: true,
         pageLength: 5,
         lengthChange: true,
         lengthMenu: [ [5, 25, 50, 100, -1], [5, 25, 50, 100, "All"] ],
         searching: true,
         ordering: true,
         responsive: true,
-        autoWidth: true
+        autoWidth: true,
+        order: [[1, 'asc']],
+        columnDefs: [
+            {
+                orderable: false,
+                render: DataTable.render.select(),
+                targets: 0
+            }
+        ],
+        select: {
+            style: 'multi',
+            selector: 'td:first-child'
+        }
     });
 </script>
 @endscript
 
-<div>
+<div wire:ignore>
     <table id="{{ $id }}" class="table display responsive table-striped" style="width:100%" >
         <thead>
             <tr>
+                <th></th>
                 <th>No</th>
             @foreach($head as $r)
                 <th>{{ $r }}</th>
@@ -46,6 +63,7 @@ $cell=collect(['name','email']);
         <tbody>
             @foreach($data as $key=>$row)
             <tr>
+                <th></th>
                 <td>{{1+$key++}}</td>
                 @foreach($cell as $v)
                 <td>{{ $row->$v}}</td>
